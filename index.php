251e5f90c0a58403b3d088b5469afec8806e66c1
ECHALAYE <etienne.chalaye@ynov.com>
2023-02-13 10:28:25 +0100

    page vente brute faite

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 $mysqli = new mysqli("localhost", "root", "", "php_exam_db"); // Connexion à la db "php_exam"
 // si vous avez une erreur ici, remplacez le deuxième "root" par une string vide
 
-$result = $mysqli->query("SELECT * FROM article"); // On utilise l'instance créée pour faire une requête bidon
+$result = $mysqli->query("SELECT name FROM article"); // On utilise l'instance créée pour faire une requête bidon
 $hello = "World";
 ?>
 <h1>Hello <?php echo $hello ?> !</h1> 
+<?php echo $result ?>
+<a href="vente.php" role="button">Vente</a>
--- vente.php
+++ vente.php
@@ -1,9 +1,14 @@
 <?php
-    $nameart = $_POST["Nameart"];
-    if($nameart != null){
+    $mysqli = new mysqli("localhost", "root", "", "php_exam_db");
+    if(isset($_POST["Nameart"])){
+        $nameart = $_POST["Nameart"];
         $desc = $_POST["Description"];
         $price = $_POST["Price"];
         $img = $_POST["Image"];
+        $stk = $_POST["Stock"];
+        $date = date('d/m/Y h:i:s', time());
+        $price = floatval($price);
+        $mysqli->query("INSERT INTO article(`name`, `description`, `prix`,`datePublie`,`img`) VALUES ('$nameart', '$desc', '$price', '$date', '$img')");
     }else{
         $nameart = "";
         $img = "";
@@ -13,13 +18,14 @@
 ?>
 
 <html>
-<h1> <?php echo "name ", $nameart, " desc ", $desc , " price " , $price , " img " , $img ?> !</h1> 
 <body>
         <form action="vente.php" method="post">
             <input type="text" name="Nameart" placeholder="Enter the name of your article." required>
             <input type="text" name="Description" placeholder="Enter a description for your article." required>
-            <input type="text" name="Price" placeholder="Enter the price of your article." required>
+            <input type="number" name="Price" placeholder="Enter the price of your article." required>
             <input type="text" name="Image" placeholder="Add image of your article." required>
+            <input type="text" name="Stock" placeholder="Quantity." required>
+            <input type="submit" href="vente.php">
         </form>
     </body>
 </html>
