73a1b3d76fd6eddf8f059fdc391d63d9e6bd0acc
ECHALAYE <etienne.chalaye@ynov.com>
2023-02-19 22:28:12 +0100

    page de detail guest

--- detail.php
+++ detail.php
@@ -4,13 +4,13 @@ if(isset($_COOKIE["pwd"])){
     $cookpass = $_COOKIE["pwd"];
     $trypwd = $mysqli->query("SELECT `mdp` FROM user WHERE `mdp` = '$cookpass'");
     if($trypwd->num_rows == 0){
-        header("Location: http://localhost/php_exam/indexGuest.php");
+        header("Location: http://localhost/php_exam/detailGuest.php");
     }else{
         $accountInfo = $mysqli->query("SELECT * FROM user WHERE `mdp` = '$cookpass'");
         $accountInfo = $accountInfo->fetch_assoc();
     }
 }else{
-    header("Location: http://localhost/php_exam/indexGuest.php");
+    header("Location: http://localhost/php_exam/detailGuest.php");
 }
 ?>
 
